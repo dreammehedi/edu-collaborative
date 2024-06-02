da6da5d13aabd4cffedac315b3dd8216eb4c8773
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-06-02 20:13:00 +0600

    Scroll To Top Component Added

--- src/layout/main_layout/MainLayout.jsx
+++ src/layout/main_layout/MainLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../../shared/footer/Footer";
 import Header from "../../shared/header/Header";
-
+import ScrollToTop from "./../../components/scroll_to_top/ScrollToTop";
 function MainLayout() {
   return (
     <>
+      <ScrollToTop></ScrollToTop>
       <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
