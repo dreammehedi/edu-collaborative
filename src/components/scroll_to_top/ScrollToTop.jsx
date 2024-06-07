57b2dcf3d38da5a0897a0d9ad049dd9b03f8bf2b
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-06-07 18:52:12 +0600

    Scroll To Top Update

--- src/components/scroll_to_top/ScrollToTop.jsx
+++ src/components/scroll_to_top/ScrollToTop.jsx
@@ -1,17 +1,43 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { FaArrowUp } from "react-icons/fa";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  // show click to top button
+  const [showClickToTop, setShowClickToTop] = useState(false);
 
   useEffect(() => {
+    const handleShow = () => {
+      if (window.scrollY > 320) {
+        setShowClickToTop(true);
+      } else {
+        setShowClickToTop(false);
+      }
+    };
+    window.addEventListener("scroll", handleShow);
+    return () => {
+      window.removeEventListener("scroll", handleShow);
+    };
+  }, []);
+
+  //   handle click to top
+  const handleClickToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
-  }, [pathname]);
-
-  return null;
+  };
+  return (
+    <>
+      {showClickToTop && (
+        <button
+          onClick={handleClickToTop}
+          className="animate-pulse text-white text-xl p-4 bg-primary/80 my-transition hover:shadow hover:shadow-primary rounded-md  hover:bg-primary fixed bottom-10 right-10 z-[9999]"
+        >
+          <FaArrowUp></FaArrowUp>
+        </button>
+      )}
+    </>
+  );
 };
 
 export default ScrollToTop;
