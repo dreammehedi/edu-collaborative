f9aa5c4b4f4d90438574dbef15dd0ce22b619f30
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-06-07 19:21:15 +0600

    Responsive Error Page

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "https://edu-collaborate.vercel.app/",
+  baseURL: "http://localhost:5000",
 });
 function useAxiosPublic() {
   return axiosPublic;
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,52 +1,46 @@
+import { FaArrowLeft } from "react-icons/fa6";
+import { Link, useNavigate } from "react-router-dom";
 function Error() {
+  let navigate = useNavigate();
+
   return (
     <>
-      <section className="bg-white dark:bg-gray-900 ">
-        <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
-          <div className="wf-ull lg:w-1/2">
-            <p className="text-sm font-medium text-blue-500 dark:text-blue-400">
-              404 error
-            </p>
-            <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">
+      <section className="bg-white ">
+        <div className="container min-h-screen px-6 py-12 mx-auto flex justify-center items-center lg:items-center lg:justify-normal lg:flex-row lg:gap-12">
+          <div className="hidden md:inline-block relative w-full mt-12 lg:w-1/2 lg:mt-0">
+            <img
+              className="w-full max-w-lg lg:mx-auto"
+              src="https://i.ibb.co/B2dBSBp/page-found-concept-illustration-114360-1869.jpgg"
+              alt=""
+            />
+          </div>
+          <div className="flex flex-col space-y-3 justify-center items-center text-center w-full lg:w-1/2">
+            <p className="text-base font-medium text-blue-500 ">404 Error</p>
+            <h1 className="mt-3 text-2xl font-semibold text-gray-800  md:text-3xl">
               Page not found
             </h1>
-            <p className="mt-4 text-gray-500 dark:text-gray-400">
-              {` Sorry, the page you are looking for doesn't exist.Here are some
+            <p className="mt-4 text-gray-500 ">
+              {` Sorry, the page you are looking for doesn't exist. Here are some
               helpful links:`}
             </p>
 
-            <div className="flex items-center mt-6 gap-x-3">
-              <button className="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto dark:hover:bg-gray-800 dark:bg-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:border-gray-700">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-5 h-5 rtl:rotate-180"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-                  />
-                </svg>
-
-                <span>Go back</span>
+            <div className="w-full flex flex-col items-center justify-center mt-6 gap-3">
+              <button
+                onClick={() => {
+                  navigate(-1);
+                }}
+                className="flex items-center justify-center px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border rounded-lg gap-x-2 sm:w-auto hover:bg-gray-100"
+              >
+                <FaArrowLeft></FaArrowLeft>
+                <span>Go Back</span>
               </button>
 
-              <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
-                Take me home
-              </button>
+              <Link to={"/"}>
+                <button className="px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 ">
+                  Take me Home
+                </button>
+              </Link>
             </div>
-          </div>
-
-          <div className="relative w-full mt-12 lg:w-1/2 lg:mt-0">
-            <img
-              className="w-full max-w-lg lg:mx-auto"
-              src="/images/components/illustration.svg"
-              alt=""
-            />
           </div>
         </div>
       </section>
